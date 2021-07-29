667bd72f5f5d63fed1ce9e2bdf90554025f27065
Sneguro4ka <yvova88@yahoo.com>
2021-07-29 13:22:04 +0300

    testing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return (
         <div>
-            <h1>Hi there.. i'm a container lol</h1>
+            <h1>Hi there.. i'm a container lol?</h1>
             <hr/>
             <MarketingApp />
         </div>
