086860253a13a0871760e9db026b65635a87bb60
Sneguro4ka <yvova88@yahoo.com>
2021-07-31 19:41:37 +0300

    configuring routes

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+
+    if ( onNavigate ) {
+        // if you remove this if, and run in isolation the marketing app, it will fail.
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App />, 
+        <App history={history}/>, 
         el
     );
-}
 
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+            if ( pathname !== nextPathname ){
+                history.push(nextPathname)
+            }
+        }
+    }
+}
+// in dev, in isolation
 if ( process.env.NODE_ENV === 'development' ) {
     const devRoot = document.querySelector('#_marketing-dev-root')
 
     if ( devRoot ) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() } );
     }
 }
 
