dcaa0fe1234f3943747881b9548bb25fc0a7caef
Sneguro4ka <yvova88@yahoo.com>
2021-08-01 13:07:37 +0300

    fixing dashboard

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -17,7 +17,7 @@ const prodConfig = {
             name: 'dashboard', // not require, but following convention
             filename: 'remoteEntry.js',
             exposes: {
-                './DashboardApp': './src/bootstrap' // marketing before @, matches the name of config.dev of marketing app
+                './DashboardApp': './src/bootstrap' // dashboard before @, matches the name of config.dev of marketing app
             },
             shared: packageJson.dependencies
         }),
