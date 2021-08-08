0f9ee4a2880454865cfdafcc29092ea93e0964e9
Sneguro4ka <yvova88@yahoo.com>
2021-08-08 11:39:57 +0300

    fixing env for dashboard

--- packages/dashboard/src/components/Dashboard.js
+++ packages/dashboard/src/components/Dashboard.js
@@ -3,6 +3,19 @@ import React from 'react';
 export default function Dashboard() {
 
   return (
-    <div>Dashboard</div>
+          <div>
+            <hr/>
+              <Suspense fallback={<div>loading UsersLazy...</div>}>
+                <UsersLazy />
+              </Suspense>
+              <hr/>
+              <Suspense fallback={<div>loading ShiftsLazy...</div>}>
+                <ShiftsLazy />
+              </Suspense>
+              <hr/>
+              <Suspense fallback={<div>loading MessagesLazy...</div>}>
+                <MessagesLazy />
+              </Suspense>
+          </div>
   );
 }
