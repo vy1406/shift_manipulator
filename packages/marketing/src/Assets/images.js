839f9c31aada7fc7586f1155df7c8145c4aca562
MilenaGkm <milenagaranin@gmail.com>
2021-11-30 18:23:13 +0200

    icons are floating

---
+++ packages/marketing/src/Assets/images.js
@@ -0,0 +1,12 @@
+import nestjs from '../assets/nestjs-logo-1.png'
+
+const images = [
+    { id: 1, src: nestjs, title: 'foo1', description: 'bar' },
+    // { id: 2, src: './assets/image02.jpg', title: 'foo2', description: 'bar' },
+    // { id: 3, src: './assets/image03.jpg', title: 'foo3', description: 'bar' },
+    // { id: 4, src: './assets/image04.jpg', title: 'foo4', description: 'bar' },
+    // { id: 5, src: './assets/image05.jpg', title: 'foo5', description: 'bar' },
+    // ...etc
+];
+
+export default images;
--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState, useEffect } from "react"
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -13,9 +14,10 @@ import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import { Link as RouterLink } from 'react-router-dom';
 
+
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography letiant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
@@ -65,187 +67,137 @@ const useStyles = makeStyles((theme) => ({
       paddingBottom: theme.spacing(6),
     },
   },
+  techList: {
+    height: '70px',
+    // width: '100px',
+    position: 'fixed',
+    top: "0px",
+    left: "0px",
+    // transition: Animation()
+  }
 }));
 
-const tiers = [
-  {
-    title: 'Free',
-    price: '0',
-    description: [
-      '10 users included',
-      '2 GB of storage',
-      'Help center access',
-      'Email support',
-    ],
-    buttonText: 'Sign up for free',
-    buttonVariant: 'outlined',
-  },
-  {
-    title: 'Pro',
-    subheader: 'Most popular',
-    price: '15',
-    description: [
-      '20 users included',
-      '10 GB of storage',
-      'Help center access',
-      'Priority email support',
-    ],
-    buttonText: 'Get started',
-    buttonVariant: 'contained',
-  },
-  {
-    title: 'Enterprise',
-    price: '30',
-    description: [
-      '50 users included',
-      '30 GB of storage',
-      'Help center access',
-      'Phone & email support',
-    ],
-    buttonText: 'Contact us',
-    buttonVariant: 'outlined',
-  },
+
+
+
+
+const TECH_LIST = [
+  { id: 0, title: "NestJS", img: 'https://seeklogo.com/images/N/nestjs-logo-09342F76C0-seeklogo.com.png' },
+  { id: 1, title: "React", img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/320px-React-icon.svg.png' },
+  { id: 2, title: "Redux-Saga", img: 'https://redux-saga.js.org//img/Redux-Saga-Logo-Portrait.png' },
+  { id: 3, title: "npm", img: 'https://cdn.freebiesupply.com/logos/thumbs/2x/npm-logo.png' },
+  { id: 4, title: "Material-UI", img: 'https://seeklogo.com/images/M/material-ui-logo-5BDCB9BA8F-seeklogo.com.png' },
+  { id: 5, title: "webpack", img: 'https://webpack.js.org/icon-pwa-512x512.d3dae4189855b3a72ff9.png' },
+  { id: 6, title: "CSS", img: 'https://cdn.freelogovectors.net/wp-content/uploads/2020/04/css-3-logo.png' },
+  { id: 7, title: "HTML", img: 'https://www.pngrepo.com/png/183637/512/html5.png' },
+  { id: 8, title: "Netlify", img: 'https://seeklogo.com/images/N/netlify-logo-758722CDF4-seeklogo.com.png' },
+  { id: 9, title: "AWS", img: 'https://logos-world.net/wp-content/uploads/2021/08/Amazon-Web-Services-AWS-Logo.png' },
+  { id: 10, title: "MongoDB Atlas", img: 'https://g.foolcdn.com/image/?url=https%3A%2F%2Fg.foolcdn.com%2Feditorial%2Fimages%2F635884%2Fatlas_icon_blk_stackedlarge.png&w=1200&op=resize' },
+  { id: 11, title: "MongoDB", img: 'https://infinapps.com/wp-content/uploads/2018/10/mongodb-logo.png' },
+  { id: 12, title: "PWA", img: 'https://responsivedesign.is/wp-content/uploads/2018/08/PWA-Progressive-Web-App-Logo.png' },
+  { id: 13, title: "Github", img: 'https://cdn.iconscout.com/icon/free/png-256/github-153-675523.png' },
+  { id: 14, title: "Jira", img: 'https://cdn.icon-icons.com/icons2/2699/PNG/512/atlassian_jira_logo_icon_170511.png' },
 ];
-const footers = [
-  {
-    title: 'Company',
-    description: ['Team', 'History', 'Contact us', 'Locations'],
-  },
-  {
-    title: 'Features',
-    description: [
-      'Cool stuff',
-      'Random feature',
-      'Team feature',
-      'Developer stuff',
-      'Another one',
-    ],
-  },
-  {
-    title: 'Resources',
-    description: [
-      'Resource',
-      'Resource name',
-      'Another resource',
-      'Final resource',
-    ],
-  },
-  {
-    title: 'Legal',
-    description: ['Privacy policy', 'Terms of use'],
-  },
-];
+
+
+
+
 
 export default function Pricing() {
   const classes = useStyles();
+ 
+  function moveDiv(divId) {
+    let max_width = window.innerWidth;
+    let max_height = window.innerHeight;
+
+    let directions = { left: 1, up: 2, right: 3, down: 4 }
+    let direction = getRandomDirection();
+    let distance = getRandomDistance();
+
+    const target = document.getElementsByClassName("techList")[divId];
+    let target_pos = { top: 100, left: 100 }
+
+    let i = 0;
+
+    let render_rate = 30;
+    let move_step = 2;
+
+    setInterval(function () {
+      i++;
+      if (i > distance) {
+        distance = getRandomDistance();
+        direction = getRandomDirection();
+        i = 0;
+      }
+      move(target, direction, move_step)
+    }, render_rate)
+
+    function getRandomDistance() {
+      return Math.floor((Math.random() * 20) + 1) + 5;
+    }
+
+    function getRandomDirection() {
+      return Math.floor((Math.random() * 4) + 1);
+    }
+
+    function move(el, direction, step) {
+      switch (direction) {
+        case directions.left: {
+          if (target_pos.left < max_width) {
+            target_pos.left += step;
+            el.style.left = target_pos.left + "px";
+          }
+          break;
+        }
+        case directions.up: {
+          if (target_pos.top < max_height) {
+            target_pos.top += step;
+            el.style.top = target_pos.top + "px";
+          }
+          break;
+        }
+
+        case directions.right: {
+          if (target_pos.left > 0) {
+            target_pos.left -= step;
+            el.style.left = target_pos.left + "px";
+          }
+          break;
+        }
+
+        case directions.down: {
+          if (target_pos.top > 0) {
+            target_pos.top -= step;
+            el.style.top = target_pos.top + "px";
+          }
+          break;
+        }
+      }
+    }
+  };
+
+
+
+  useEffect(() => {
+    for (let i of TECH_LIST) {
+      moveDiv(i.id);
+      // console.log(i);
+    }
+  })
+
 
   return (
     <React.Fragment>
-      {/* Hero unit */}
-      <Container maxWidth="sm" component="main" className={classes.heroContent}>
-        <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="textPrimary"
-          gutterBottom
-        >
-          Pricing
-        </Typography>
-        <Typography
-          variant="h5"
-          align="center"
-          color="textSecondary"
-          component="p"
-        >
-          Quickly build an effective pricing table for your potential customers
-          with this layout. It&apos;s built with default Material-UI components
-          with little customization.
-        </Typography>
-      </Container>
-      {/* End hero unit */}
-      <Container maxWidth="md" component="main">
-        <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
-            // Enterprise card is full width at sm breakpoint
-            <Grid
-              item
-              key={tier.title}
-              xs={12}
-              sm={tier.title === 'Enterprise' ? 12 : 6}
-              md={4}
-            >
-              <Card>
-                <CardHeader
-                  title={tier.title}
-                  subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{ align: 'center' }}
-                  action={tier.title === 'Pro' ? <StarIcon /> : null}
-                  className={classes.cardHeader}
-                />
-                <CardContent>
-                  <div className={classes.cardPricing}>
-                    <Typography component="h2" variant="h3" color="textPrimary">
-                      ${tier.price}
-                    </Typography>
-                    <Typography variant="h6" color="textSecondary">
-                      /mo
-                    </Typography>
-                  </div>
-                  <ul>
-                    {tier.description.map((line) => (
-                      <Typography
-                        component="li"
-                        variant="subtitle1"
-                        align="center"
-                        key={line}
-                      >
-                        {line}
-                      </Typography>
-                    ))}
-                  </ul>
-                </CardContent>
-                <CardActions>
-                  <Button
-                    component={RouterLink}
-                    to="/auth/signup"
-                    fullWidth
-                    color="primary"
-                  >
-                    {tier.buttonText}
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-      {/* Footer */}
-      <Container maxWidth="md" component="footer" className={classes.footer}>
-        <Grid container spacing={4} justify="space-evenly">
-          {footers.map((footer) => (
-            <Grid item xs={6} sm={3} key={footer.title}>
-              <Typography variant="h6" color="textPrimary" gutterBottom>
-                {footer.title}
-              </Typography>
-              <ul>
-                {footer.description.map((item) => (
-                  <li key={item}>
-                    <Link href="#" variant="subtitle1" color="textSecondary">
-                      {item}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Grid>
-          ))}
-        </Grid>
-        <Box mt={5}>
-          <Copyright />
-        </Box>
-      </Container>
-      {/* End footer */}
+
+      <div>
+        {TECH_LIST.map((img, i) => (
+          <div >
+            <img className="techList" style={{ position: "fixed", height: "100px", top: "100px", left: "100px" }} key={img.id} src={img.img} />
+          </div>
+        )
+        )}
+      </div>
+
     </React.Fragment>
   );
 }
